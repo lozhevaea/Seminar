1da001e84f76696fb7de3d5a24cdbb01af5cfa0a
Elena Kuznetsova <lozhevaea@gmail.com>
2023-05-28 12:32:22 +0500

    add solution

--- Seminar_04/task24/Program.cs
+++ Seminar_04/task24/Program.cs
@@ -3,3 +3,37 @@
 // 4 -> 10
 // 8 -> 36
 
+
+// System.Console.Write("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// for (int i = 1; i <= A; i++)
+// {
+//     sum+=i; //sum = sum + i;
+// }
+// System.Console.WriteLine(sum);
+
+
+
+//Решение через функции
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sumnambers(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum += i; //sum = sum + i;
+    }
+    return sum;
+}
+
+int A = ReadInt("Введите число: ");
+int sum = Sumnambers(A);
+System.Console.WriteLine(sum);
