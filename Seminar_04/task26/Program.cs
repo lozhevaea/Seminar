b3aef500895c3a5f866cd2a4504eee6e28a83840
Elena Kuznetsova <lozhevaea@gmail.com>
2023-05-28 13:20:01 +0500

    add task26

---
+++ Seminar_04/task26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNambers(int A)
+{
+    int sum = 0;
+    while (A != 0)
+    {
+        A /= 10;
+        sum += 1;
+    }
+    return sum;
+}
+
+int A = ReadInt("Введите число: ");
+System.Console.WriteLine(CountNambers(A));
