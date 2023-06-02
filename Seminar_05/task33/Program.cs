8a7e0d2077c317d5184b76c5a0cdf84a1ca92bc1
Elena Kuznetsova <lozhevaea@gmail.com>
2023-06-02 22:15:55 +0500

    add task35

---
+++ Seminar_05/task33/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, -3] -> да
+
+void Main()
+{
+    int Num = ReadInt("Введите число для поиска: ");
+    int[] arr = FillArray(5, 1, 10);
+    PrintArray(arr);
+    System.Console.WriteLine(CheckIfInArr(arr, Num));
+}
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    int[] Array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return Array;
+}
+void PrintArray(int[] Array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", Array) + "]");
+}
+
+bool CheckIfInArr(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num)
+        {
+            return true;
+        }
+     return false;
+    }
+}
+
+// int ChackIfInArr(int[] arr, int num)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] == num)
+//         {
+//            return System.Console.WriteLine("Да");
+//         }
+//         return System.Console.WriteLine("Нет");
+//     }
+// }
+
+// Main();
+
+
+// int[] arr = {3, 4, 8, 6};
+// System.Console.WriteLine(Array.IndexOf(arr, 5));
---
+++ Seminar_05/task35/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99]. 
+
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    int[] Array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return Array;
+}
+
+void PrintArray(int[]Array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", Array) + "]");
+}
+
+int CountElem(int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] >= 10 && Array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] arr = FillArray(10, -90, 100);
+PrintArray(arr);
+System.Console.WriteLine($"Количество чисел, входящих в диапазон от 10 до 99: {CountElem(arr)}");
