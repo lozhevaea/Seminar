5dde0709dce4a97a6a4e808bb829c0269a5d1425
Elena Kuznetsova <lozhevaea@gmail.com>
2023-06-04 17:50:40 +0500

    add task44

---
+++ Seminar_06/task42/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// 3 -> 11
+// 2 -> 10
+// 44 -> 101100
+
+void Main()
+{
+    int N = ReadInt("Введите число: ");
+    // ConvertToBinary(N);
+    string str = Convert.ToString(N, 2);
+    System.Console.WriteLine(str);
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ConvertToBinary(int N)
+{
+    if (N <= 0)
+    {
+        return;
+    }
+    ConvertToBinary(N / 2); // если поменять местами 27 и 28 строки, то будет печатать в обратном порядке.
+    System.Console.Write(N % 2);
+}
+
+Main();
---
+++ Seminar_06/task44/Program.cs
@@ -0,0 +1,33 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 3 -> 0 1 1
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+void Main()
+{
+    PrintFibonachi(ReadInt("Введите число элементов функции Фибоначчи: "));
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintFibonachi(int num)
+{
+    System.Console.Write("0 1 ");
+    int A  = 0;
+    int B = 1;
+    for (int i = 0; i < num-2; i++)
+    {
+        int res = A + B;
+        System.Console.Write(res + " ");
+        A = B;
+        B = res;
+    }
+}
+
+Main();
