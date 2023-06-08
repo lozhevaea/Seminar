eb21638f57e0f07176ebe5e2e83f7eb093086964
Elena Kuznetsova <lozhevaea@gmail.com>
2023-06-08 23:27:46 +0500

    add task48

---
+++ Seminar_07/task48/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размера m*n, каждый элемент в массиве находится по формуле A[mn] = m + n
+// Выведите этот массив на  экран
+
+// rows = 3 cols = 4
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void Main()
+{
+    int[,] matrix = FillMatrix(ReadInt("Введите количество строк: "), ReadInt("Введите количество столбцов: "));
+    PrintMatrix(matrix);
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int row, int col)
+{
+    int[,] matrix = new int[row, col];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t"); 
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Main();
