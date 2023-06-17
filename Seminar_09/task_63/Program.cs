a4091593dfca78c2f0750c7503c797012759f684
Elena Kuznetsova <lozhevaea@gmail.com>
2023-06-17 22:11:42 +0500

    add task63

---
+++ Seminar_09/task_63/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежетке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Numbers(int N)
+{
+    if (N < 1) return;
+    Numbers(N - 1);
+    System.Console.Write(N + " ");  
+    
+}
+
+Numbers(ReadInt("Введите значение N: "));
