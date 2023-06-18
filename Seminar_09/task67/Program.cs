f50541fafc1edc3f52690531130ab6248bda0dff
Elena Kuznetsova <lozhevaea@gmail.com>
2023-06-18 10:09:44 +0500

    add task67

---
+++ Seminar_09/task67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+// 453 -> 12
+// 45 -> 9
+
+int SumNumbers(int n)
+{
+    if (n == 0) return 0;
+    return n%10 + SumNumbers(n/10);
+}
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+System.Console.WriteLine(SumNumbers(ReadInt("Введите число: ")));
